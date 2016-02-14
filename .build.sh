40e84449fd735e97a67b7f5cc2f4e3c4385ec6cc
Markus Tacker <m@cto.hiv>
2016-02-14 17:51:50 +0100

    fix(styles): remove hard-coded non-ssl from font loading

--- .build.sh
+++ .build.sh
@@ -3,10 +3,7 @@
 # Set abort on error
 set -e
 
-# Install deps
-bundle install --path .bundle
-
 # Build site
 rm -rf "${TARGET_DIR:-_site}"/*
-bundle exec jekyll build -d "${TARGET_DIR:-_site}" --config _config.yml
+jekyll build -d "${TARGET_DIR:-_site}" --config _config.yml
 
