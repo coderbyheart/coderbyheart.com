6777ec061a7ee4779dccd14f96c6a2d28bde4086
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-10-14 09:37:24 +0200

    fix: increase avatar size

--- src/design/Footer.tsx
+++ src/design/Footer.tsx
@@ -106,7 +106,7 @@ const Photo = styled.img`
 `
 
 export const avatarUrl =
-	'https://images.contentful.com/bncv3c2gt878/6CWMgqeZdCmkk6KkIUksgQ/50922090bc6566c6624c12b82a4bf78c/36671282034_427eace68d_o.jpg?w=150'
+	'https://images.contentful.com/bncv3c2gt878/6CWMgqeZdCmkk6KkIUksgQ/50922090bc6566c6624c12b82a4bf78c/36671282034_427eace68d_o.jpg?w=300'
 
 export const Footer = ({
 	siteMetaData: { title },
