d071612aac6d2ff9f2573505b810c676f7cb6381
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-10-15 00:01:46 +0200

    feat: add support for multiple archive entries

--- src/design/Title.tsx
+++ src/design/Title.tsx
@@ -1,43 +1,32 @@
 import React from 'react'
 import { Page } from '../site'
 import styled from 'styled-components'
-import { formatDistanceToNow } from 'date-fns'
+import { format } from 'date-fns'
 
 const Subtitle = styled.div`
-	span + time {
-		:before {
-			content: '·';
-			padding: 0 0.5rem;
-		}
-	}
+	font-family: var(--headline-font-family);
+	font-weight: var(--headline-font-weight-thin);
+`
+
+const Time = styled.time`
 	font-family: var(--headline-font-family);
 	font-weight: var(--headline-font-weight-thin);
 `
 
 const Wrapper = styled.div`
 	display: flex;
-	flex-direction: column-reverse;
+	flex-direction: column;
+	h1 {
+		margin-top: 0.5rem;
+	}
 `
 
-export const Title = ({ page }: { page: Page }) => {
-	const subtitle = []
-	if (page.remark.frontmatter.subtitle)
-		subtitle.push(<span>{page.remark.frontmatter.subtitle}</span>)
-	if (page.remark.frontmatter.date)
-		subtitle.push(
-			<time
-				dateTime={page.remark.frontmatter.date}
-				title={new Date(page.remark.frontmatter.date).toLocaleDateString()}
-			>
-				{formatDistanceToNow(new Date(page.remark.frontmatter.date), {
-					addSuffix: true,
-				})}
-			</time>,
-		)
-	return (
-		<Wrapper>
-			<h1>{page.remark.frontmatter.title}</h1>
-			{subtitle.length > 0 && <Subtitle>{subtitle}</Subtitle>}
-		</Wrapper>
-	)
-}
+export const Title = ({ page }: { page: Page }) => (
+	<Wrapper>
+		<Subtitle>{page.remark.frontmatter.subtitle}</Subtitle>
+		<h1>{page.remark.frontmatter.title}</h1>
+		<Time dateTime={page.remark.frontmatter.date}>
+			{format(new Date(page.remark.frontmatter.date), 'd. MMMM yyyy')}
+		</Time>
+	</Wrapper>
+)
